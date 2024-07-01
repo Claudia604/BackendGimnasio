1341950b0722caf765f1959e7a63d3efff02e27e
Claudia Torres <alet24665@gmail.com>
2024-06-30 23:35:26 -0300

    se installo express-validator

--- src/routes/clases.routes.js
+++ src/routes/clases.routes.js
@@ -6,11 +6,12 @@ import {
   listarClase,
   obtenerClase,
 } from "../controllers/clases.controllers.js";
+import { check } from "express-validator";
 
 const router = Router();
 
 router.route("/gym").get(listarClase);
-router.route("/clases").post(crearClase).get(listarClase);
+router.route("/clases").post([],crearClase).get(listarClase);
 router
   .route("/clases/:id")
   .get(obtenerClase)
